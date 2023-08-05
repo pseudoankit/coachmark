a90bf607d24f4af14db747eebeb7a4e841867900
ankitkumar <lostankit7@gmail.com>
2023-08-06 00:22:47 +0530

    attempt 2 , made coachmark more scalable

--- coachmark/src/main/java/com/pseudoankit/coachmark/UnifyCoachmark.kt
+++ coachmark/src/main/java/com/pseudoankit/coachmark/UnifyCoachmark.kt
@@ -2,10 +2,9 @@ package com.pseudoankit.coachmark
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -14,45 +13,69 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.layout.positionInRoot
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.pseudoankit.coachmark.internal.toDp
 
-@androidx.compose.runtime.Composable
-fun UnifyCoachmark(
-    showCoachMark: Boolean,
-    content: @Composable () -> Unit
-) {
-    Box {
-        var coachMarkPosition by remember { mutableStateOf(Offset(0f, 0f)) }
-        Box(
-            modifier = Modifier
-                .onGloballyPositioned {
-                    val position = it.positionInRoot()
-                    val size = it.size
-                    coachMarkPosition = Offset(x = position.x, y = position.y + size.height)
-                }
-        ) {
-            content()
-        }
+data class CoachMarkItem(
+    val text: String,
+    val coordinates: Offset,
+    val show: Boolean
+)
 
-        if (showCoachMark) {
+class CoachMark<T> {
+    private val items = mutableMapOf<T, CoachMarkItem>()
+    var activeCoachmark by mutableStateOf<CoachMarkItem?>(null)
+
+    fun Modifier.enableCoachMark(
+        text: String,
+        type: T
+    ): Modifier = composed {
+        onGloballyPositioned {
+            val coordinates = Offset(
+                x = it.positionInRoot().x,
+                y = it.positionInRoot().y + it.size.height
+            )
+
+            items[type] = CoachMarkItem(text, coordinates, false)
+        }
+    }
+
+    fun hide(item: T) {
+        activeCoachmark = null
+    }
+
+    fun reveal(item: T) {
+        activeCoachmark =
+            items[item] ?: throw NotImplementedError("coachmark not enabled for the type $item")
+    }
+}
+
+
+@Composable
+fun <T> UnifyCoachmark(
+    content: @Composable CoachMark<T>.() -> Unit
+) {
+    val coachMark = remember { CoachMark<T>() }
+    val activeCoachMark = coachMark.activeCoachmark
+
+    Box {
+        content(coachMark)
+        if (activeCoachMark != null) {
             Text(
-                text = "Coachmark ..",
+                text = activeCoachMark.text,
+                color = Color.White,
                 modifier = Modifier
                     .offset(
-                        coachMarkPosition.x.toDp(LocalDensity.current),
-                        coachMarkPosition.y.toDp(LocalDensity.current)
+                        activeCoachMark.coordinates.x.toDp(LocalDensity.current),
+                        activeCoachMark.coordinates.y.toDp(LocalDensity.current)
                     )
-                    .wrapContentSize()
                     .background(Color.Blue)
-                    .padding(horizontal = 16.dp, vertical = 8.dp),
-                color = Color.White
             )
         }
     }
@@ -63,14 +86,25 @@ fun UnifyCoachmark(
 fun UnifyCoachmarkDemo() {
     Box(modifier = Modifier.fillMaxSize()) {
 
-        var showCoachMark by remember { mutableStateOf(false) }
-        UnifyCoachmark(showCoachMark = showCoachMark) {
-            Button(
-                onClick = {
-                    showCoachMark = true
+        UnifyCoachmark<Int> {
+            Column {
+                Button(
+                    onClick = {
+                        reveal(0)
+                    },
+                    modifier = Modifier.enableCoachMark("text1", 0)
+                ) {
+                    Text(text = "Click me")
                 }
-            ) {
-                Text(text = "Click me")
+
+                Button(
+                    onClick = {
+                        reveal(1)
+                    },
+                    modifier = Modifier.enableCoachMark("text22", 1)
+                ) {
+                    Text(text = "Click me")
+                }
             }
         }
     }
