e3ba46a68885a066b10eb0e5b893351390cf06f8
ankitkumar <lostankit7@gmail.com>
2023-09-03 17:22:44 +0530

    updated demo

--- coachmark/src/main/java/com/pseudoankit/coachmark/demo/UnifyCoachmarkDemo.kt
+++ coachmark/src/main/java/com/pseudoankit/coachmark/demo/UnifyCoachmarkDemo.kt
@@ -1,19 +1,15 @@
 package com.pseudoankit.coachmark.demo
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -25,7 +21,8 @@ import com.pseudoankit.coachmark.overlay.DimOverlayEffect
 import com.pseudoankit.coachmark.shape.Arrow
 import com.pseudoankit.coachmark.shape.Balloon
 import com.pseudoankit.coachmark.util.CoachMarkKey
-import com.pseudoankit.coachmark.util.clickable
+
+public enum class Keys { Text1, Text2 }
 
 @Preview
 @Composable
@@ -36,32 +33,54 @@ public fun UnifyCoachmarkDemo() {
         onOverlayClicked = { OverlayClickEvent.GoNext }
     ) {
         Column(
-            verticalArrangement = Arrangement.spacedBy(50.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier.fillMaxSize()
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(12.dp),
+            verticalArrangement = Arrangement.spacedBy(12.dp)
         ) {
-            Spacer(modifier = Modifier.height(12.dp))
-
-            ToolTipPlacement.values().forEach {
-                Text(
-                    text = it.name,
-                    modifier = Modifier
-                        .clip(RoundedCornerShape(12.dp))
-                        .clickable {
-                            show(*ToolTipPlacement.values())
-                        }
-                        .background(Color.Red.copy(alpha = .5f))
-                        .enableCoachMark(
-                            key = it,
-                            toolTipPlacement = it,
-                            highlightedViewConfig = HighlightedViewConfig(
-                                shape = HighlightedViewConfig.Shape.Rect(12.dp),
-                                padding = PaddingValues(8.dp)
-                            )
+            Text(
+                text = "Will show tooltip 1",
+                modifier = Modifier
+                    .enableCoachMark(
+                        key = Keys.Text1,
+                        toolTipPlacement = ToolTipPlacement.End,
+                        highlightedViewConfig = HighlightedViewConfig(
+                            shape = HighlightedViewConfig.Shape.Rect(12.dp),
+                            padding = PaddingValues(8.dp)
                         )
-                        .padding(16.dp),
-                    color = Color.White
-                )
+                    )
+                    .padding(16.dp),
+                color = Color.Black
+            )
+            Text(
+                text = "Will show tooltip 2",
+                modifier = Modifier
+                    .enableCoachMark(
+                        key = Keys.Text2,
+                        toolTipPlacement = ToolTipPlacement.End,
+                        highlightedViewConfig = HighlightedViewConfig(
+                            shape = HighlightedViewConfig.Shape.Rect(12.dp),
+                            padding = PaddingValues(8.dp)
+                        )
+                    )
+                    .padding(16.dp),
+                color = Color.Black
+            )
+            Button(
+                onClick = {
+                    show(Keys.Text1)
+                },
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            ) {
+                Text(text = "Highlight1")
+            }
+            Button(
+                onClick = {
+                    show(Keys.Text1, Keys.Text2)
+                },
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            ) {
+                Text(text = "Highlight 1 & 2")
             }
         }
     }
@@ -70,27 +89,15 @@ public fun UnifyCoachmarkDemo() {
 @Composable
 private fun Tooltip(key: CoachMarkKey) {
     when (key) {
-        ToolTipPlacement.Start -> {
-            Balloon(arrow = Arrow.End()) {
-                Text(text = "Start", color = Color.White)
-            }
-        }
-
-        ToolTipPlacement.End -> {
+        Keys.Text1 -> {
             Balloon(arrow = Arrow.Start()) {
-                Text(text = "End", color = Color.White)
+                Text(text = "Highlighting Text1", color = Color.White)
             }
         }
 
-        ToolTipPlacement.Top -> {
-            Balloon(arrow = Arrow.Bottom()) {
-                Text(text = "Top", color = Color.White)
-            }
-        }
-
-        ToolTipPlacement.Bottom -> {
-            Balloon(arrow = Arrow.Top()) {
-                Text(text = "Bottom", color = Color.White)
+        Keys.Text2 -> {
+            Balloon(arrow = Arrow.Start()) {
+                Text(text = "Highlighting Text2", color = Color.White)
             }
         }
     }
