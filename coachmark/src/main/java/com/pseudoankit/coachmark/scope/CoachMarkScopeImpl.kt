4b8e5c8caa2523bb4c8b3ece02f7842b8f2fd37d
ankitkumar <lostankit7@gmail.com>
2023-12-03 00:02:16 +0530

    set visible tooltips to empty list, when resetting index to avoid showing last visible tooltip

--- coachmark/src/main/java/com/pseudoankit/coachmark/scope/CoachMarkScopeImpl.kt
+++ coachmark/src/main/java/com/pseudoankit/coachmark/scope/CoachMarkScopeImpl.kt
@@ -30,6 +30,7 @@ internal class CoachMarkScopeImpl(
 
     private var _visibleTooltips = listOf<TooltipConfig>()
         set(value) {
+            field = emptyList()
             _visibleTooltipIndex = 0
             updateVisibleItem(value, _visibleTooltipIndex)
             field = value
