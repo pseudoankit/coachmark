c04c0ed966693bf811338f47fb6100eed52249d4
ankitkumar <lostankit7@gmail.com>
2024-02-21 03:25:12 +0530

    added ios project for demo

---
+++ demo-ios/demo-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  demo-ios
+//
+//  Created by Ankit Kumar on 21/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
